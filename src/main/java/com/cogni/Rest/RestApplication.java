25644ef32d84e1449a9419de28e95ec614ec428a
729485 <729485@PC386399.cts.com>
2018-12-17 20:48:40 +0530

    # WARNING: head commit changed in the meantime
    app

--- src/main/java/com/cogni/Rest/RestApplication.java
+++ src/main/java/com/cogni/Rest/RestApplication.java
@@ -1,29 +1,15 @@
 package com.cogni.Rest;
 
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestClientException;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RestApplication {
 
-	public static void main(String[] args) throws RestClientException, IOException {
-		ApplicationContext ctx = SpringApplication.run(
-				RestApplication.class, args);
-		
-		ConsumerClient consumerControllerClient=ctx.getBean(ConsumerClient.class);
-		System.out.println(consumerControllerClient);
-		consumerControllerClient.getEmployee();
-		
-	}
-	
-	@Bean
-	public  ConsumerClient  consumerControllerClient()
-	{
-		return  new ConsumerClient();
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
 	}
 }
